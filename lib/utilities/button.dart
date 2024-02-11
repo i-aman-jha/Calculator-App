dd5d553ab336eb8fd1f7a9c89052a562744928e3
i-aman-jha <150509057+i-aman-jha@users.noreply.github.com>
2024-02-11 12:59:45 +0530

    created button widget in separate file

--- lib/utilities/button.dart
+++ lib/utilities/button.dart
@@ -1,69 +1,75 @@
 import 'package:flutter/material.dart';
 
-
 class Mbutton extends StatelessWidget {
   final String text;
   final bool operator;
-  // Function onPressed;
+  final VoidCallback? onPressed;
 
-  Mbutton({super.key,required this.text,
-   required this.operator,
-  //  required this.onPressed,
-   });
+  const Mbutton({
+    Key? key,
+    required this.text,
+    required this.operator,
+    this.onPressed,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(MediaQuery.of(context).size.width*0.01),
+      margin: EdgeInsets.all(MediaQuery.of(context).size.width * 0.01),
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
-          backgroundColor: operator?Colors.blueAccent:Colors.white,
-          // onPrimary: Colors.white,
+          backgroundColor: operator ? Colors.blueAccent : Color.fromARGB(255, 249, 249, 249),
           shape: const CircleBorder(),
-          fixedSize: Size(MediaQuery.of(context).size.width*0.21, MediaQuery.of(context).size.width*0.21),
-          
-        ) ,
-        onPressed: 
-        (){},
-        // onPressed(), 
-        child: Text(text, style: TextStyle(color: const Color.fromARGB(255, 0, 0, 0),
-        fontSize: MediaQuery.of(context).size.width*0.07
+          fixedSize: Size(MediaQuery.of(context).size.width * 0.21, MediaQuery.of(context).size.width * 0.21),
         ),
-        )),
+        onPressed: onPressed,
+        child: Text(
+          text,
+          style: TextStyle(
+            color: const Color.fromARGB(255, 0, 0, 0),
+            fontSize: MediaQuery.of(context).size.width * 0.07,
+          ),
+        ),
+      ),
     );
   }
 }
 
-class specialbutton extends StatelessWidget {
+class Specialbutton extends StatelessWidget {
   final String text;
   final double width;
   final double height;
   final Color color;
-  Function onPressed;
-  specialbutton({super.key,
+  final VoidCallback? onPressed;
+
+  const Specialbutton({
+    Key? key,
     required this.text,
     required this.width,
     required this.height,
     required this.color,
-    required this.onPressed,
-    });
+    this.onPressed,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(MediaQuery.of(context).size.width*0.025),
+      margin: EdgeInsets.all(MediaQuery.of(context).size.width * 0.025),
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
+          elevation: 20,
           backgroundColor: color,
-          // onPrimary: Colors.white,
-          shape:RoundedRectangleBorder(
-            borderRadius:BorderRadius.circular(40)
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(40),
           ),
           fixedSize: Size(width, height),
-          
-        ) ,
-        onPressed: (){}, 
-        child: Text(text, style: TextStyle(color: const Color.fromARGB(255, 0, 0, 0),fontSize: MediaQuery.of(context).size.width*0.07),)),
+        ),
+        onPressed: onPressed,
+        child: Text(
+          text,
+          style: TextStyle(color: const Color.fromARGB(255, 0, 0, 0), fontSize: MediaQuery.of(context).size.width * 0.07),
+        ),
+      ),
     );
   }
 }
